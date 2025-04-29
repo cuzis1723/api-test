b997a74102e48331603481f948bea312e958d38b
Jiyeong Hwang <jake.h.1723@gmail.com>
2025-04-29 23:15:34 +0900

    test

--- backend/api_heavy/main.py
+++ backend/api_heavy/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "Worlds"}
+    return {"Hello": "Worldss"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
